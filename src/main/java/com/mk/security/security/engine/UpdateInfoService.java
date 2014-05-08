f1f779c7032e09c2487c0f1e8924af198896c23c
mk <monkey2562@163.com>
2014-05-08 17:37:02 +0800

    update

---
+++ src/main/java/com/mk/security/security/engine/UpdateInfoService.java
@@ -0,0 +1,18 @@
+package com.mk.security.security.engine;
+
+import android.content.Context;
+
+/**
+ * Created by Administrator on 2014/5/8.
+ */
+public class UpdateInfoService {
+    private Context context;
+
+    public UpdateInfoService(Context context)
+    {
+        this.context = context;
+    }
+
+
+
+}
