25b53bba940c43f43c612f816a259097b8a4a26c
mk <monkey2562@163.com>
2014-05-09 17:44:48 +0800

    three

---
+++ src/main/java/com/mk/security/security/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mk.security.security.ui;
+
+import com.mk.security.security.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
